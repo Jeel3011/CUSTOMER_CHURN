3975cc0522a48e59cffa05c576ad267806f11260
Jeel3011 <jeel15thummar@gmail.com>
2025-12-24 21:03:59 +0530

    2

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from src.exeption import CustomException
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        pd.to_pickle(obj, file_path)
+    except Exception as e:
+        raise CustomException(f"Error saving object: {e}", sys)
+
+def load_object(file_path):
+    try:
+        obj = pd.read_pickle(file_path)
+        return obj
+    except Exception as e:
+        raise CustomException(f"Error loading object: {e}", sys)
+
+
+
+    except Exception as e:
+        raise CustomException(e,sys)
